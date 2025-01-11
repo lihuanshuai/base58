fd04e51abd92c38155b473f6ea36556bbe3dfb28
lihuanshuai <lihuanshuaiplus@qq.com>
2025-01-11 16:53:14 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+<!--
+ Copyright 2025 lihuanshuai
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+-->
+
+# base58
+
+[Zig](https://ziglang.org/) implementation of base58 encoding
+
+## Build
+
+```bash
+zig build
+```
+
+## Run
+
+```bash
+zig run src/main.zig
+```
+
+## Test
+
+```bash
+zig test src/main.zig
+```
+
+## Reference
+
+- [base58](https://en.wikipedia.org/wiki/Base58)
---
+++ build.zig
@@ -0,0 +1,40 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    // 创建库
+    const lib = b.addStaticLibrary(.{
+        .name = "base58",
+        .root_source_file = .{ .cwd_relative = "src/base58.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    b.installArtifact(lib);
+
+    // 创建示例程序
+    const exe = b.addExecutable(.{
+        .name = "base58-example",
+        .root_source_file = .{ .cwd_relative = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    b.installArtifact(exe);
+
+    // 运行示例程序
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+    const run_step = b.step("run", "Run the example");
+    run_step.dependOn(&run_cmd.step);
+
+    // 单元测试
+    const main_tests = b.addTest(.{
+        .root_source_file = .{ .cwd_relative = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    const run_main_tests = b.addRunArtifact(main_tests);
+    const test_step = b.step("test", "Run library tests");
+    test_step.dependOn(&run_main_tests.step);
+}
